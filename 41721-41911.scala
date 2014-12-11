f81dd87b91a9d21fb48ee9f9091153aa221b7f76
pefido <pedrofilipeduraes@hotmail.com>
2014-12-11 17:28:37 +0000

    ex1

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -1,43 +1,52 @@
 object Proj3 {
+import scala.actors.Actor
+import scala.actors.Actor._
 
-  // @ARG(1) @PARTS(4) @REDUCE(sum)
-  def sum(a: Array[Double]): Double = {
-    var s: Double = 0.0
-    for( i <- a.indices )
-      s += a(i)
-      s
+  case class PartSum(num: Double);
+  case class PartOrder(a: Array[Double]);
+
+  class adder() extends Actor {
+    def act(){
+      react{
+        case PartOrder(a) =>
+          var total: Double = 0.0;
+          for(i <- a.indices)
+            total += a(i);
+          sender ! PartSum(total);
+          exit();
+      }
+      
+    }
   }
 
-  // @ARG(1) @PARTS(8) @REDUCE((x: Array[Array[Int]]) => x.flatten)
-  def increment(a: Array[Int]): Array[Int] = {
-    val res: Array[Int] = Array.ofDim[Int](a.length)
-    for( i <- a.indices )
-      res(i) = a(i) + 1
-      res
-  }
+  class joinner(a: Array[Double]) extends Actor {
+    def act(){
+      var temp = a.grouped(a.size/4).toArray;
+      for(i <- temp){
+        new adder().start() ! PartOrder(i);
+      }
 
-  // @ARG(1) @PARTS(10) @REDUCE((x: Array[Array[Array[Double]]]) => x.flatten)
-  def multiplication(a: Array[Array[Double]], b: Array[Array[Double]]):
-  Array[Array[Double]] = {
-    val res: Array[Array[Double]] =
-      Array.ofDim[Double](a.length, b(0).length)
-    for( i <- a.indices )
-      for( j <- b(0).indices )
-        for( k <- b.indices )
-          res(i)(j) += a(i)(k) * b(k)(j)
-          res
-  }
+        var finale: Double = 0.0;
+        var parts: Int = 0;
+        loop{
+          react{
+            case PartSum(total) =>
+            finale = finale + total;
+            parts = parts + 1;
+            if(parts == 4){
+              println(finale);
+              exit();
+            }
+          }
+        }
+      }
+    }
 
-  def main(args: Array[String]) = {   // testing...
-    val size = args(0).toInt
-    val a: Array[Int] = (1 to size).toArray
-    val b: Array[Double] = a map (_ * 1.0)
-    val c: Array[Double] = b map (x => (x / 2).toInt.toDouble)
-    val d: Array[Double] = Array.fill(size)(math.random)
-    val e: Array[Array[Double]] = Array.fill(size)(b)
-    val f: Array[Array[Double]] = Array.fill(size)(c)
-    println(sum(b))
-    println(runtime.ScalaRunTime.stringOf(increment(a)))
-    println(runtime.ScalaRunTime.stringOf(multiplication(e, f)))
+  def main(args: Array[String]): Unit = {
+    val size = args(0).toInt;
+    val a: Array[Int] = (1 to size).toArray;
+    val b: Array[Double] = a map (_ * 1.0);
+    val j = new joinner(b);
+    j.start();
   }
 }
