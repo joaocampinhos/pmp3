5fd03f53ce0781bfb99d2a91efe91862f1ccb5b3
pefido <pedrofilipeduraes@hotmail.com>
2014-12-12 15:05:22 +0000

    metodo sum

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -4,6 +4,8 @@ import scala.actors.Actor._
 
   case class PartSum(num: Double);
   case class PartOrder(a: Array[Double]);
+  case class PartArray(a: Array[Double]);
+  case class SumResult(d: Double);
 
   class adder() extends Actor {
     def act(){
@@ -19,34 +21,49 @@ import scala.actors.Actor._
     }
   }
 
-  class joinner(a: Array[Double]) extends Actor {
+  class partitioner extends Actor {
     def act(){
-      var temp = a.grouped(a.size/4).toArray;
-      for(i <- temp){
-        new adder().start() ! PartOrder(i);
-      }
-
+        var client: scala.actors.OutputChannel[Any] = null;
         var finale: Double = 0.0;
         var parts: Int = 0;
         loop{
           react{
             case PartSum(total) =>
-            finale = finale + total;
-            parts = parts + 1;
-            if(parts == 4){
-              println(finale);
-              exit();
-            }
+              finale = finale + total;
+              parts = parts + 1;
+              if(parts == 4){
+                //println("finale");
+                client ! SumResult(finale);
+                exit();
+              }
+            case PartArray(a) =>
+            client = sender;
+              var temp = a.grouped(a.size/4).toArray;
+              for(i <- temp)
+                new adder().start() ! PartOrder(i);
           }
         }
       }
+    }
+
+    // @ARG(1) @PARTS(4) @REDUCE(sum)
+    def sum(a: Array[Double]): Double = {
+      var result: Double = 0.0;
+      val p = new partitioner();
+      p.start();
+      p !? PartArray(a) match {
+        case SumResult(d) => 
+          result = d;
+      }
+      return result;
     }
 
   def main(args: Array[String]): Unit = {
     val size = args(0).toInt;
     val a: Array[Int] = (1 to size).toArray;
     val b: Array[Double] = a map (_ * 1.0);
-    val j = new joinner(b);
-    j.start();
+    println(sum(b));
+    //val j = new joinner(b);
+    //j.start();
   }
 }
